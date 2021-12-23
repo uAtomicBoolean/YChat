0c98fb0a39670b3ef918d383762bb981ce50580d
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2021-12-23 01:49:30 +0100

    Modification d'indentation. Début de l'affichage des messages dans une liste.

--- src/gui/AffichageMessages.java
+++ src/gui/AffichageMessages.java
@@ -2,21 +2,27 @@ package gui;
 
 import message.Message;
 
-import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.DefaultListModel;
 
 
 public class AffichageMessages extends JPanel
 {
 	private YChat fnt;
-	private JLabel affMess;
+
+	private DefaultListModel<Message> messList;
+	private JList<Message> affMess;
 
 
 	public AffichageMessages( YChat fnt )
 	{
 		this.fnt = fnt;
 
-		this.affMess = new JLabel( "no messages" );
+		this.messList = new DefaultListModel<Message>();
+
+		this.affMess = new JList<Message>( this.messList );
+		this.affMess.setDragEnabled( true );
 
 		this.add( this.affMess );
 	}
@@ -24,6 +30,6 @@ public class AffichageMessages extends JPanel
 
 	public void updateMessage( Message msg )
 	{
-		this.affMess.setText( msg.getContent() );
+		this.messList.addElement( msg );
 	}
 }
